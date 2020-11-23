095fcf3842aa5ce5fcc515ffff08a45fa5532e17
peni26553 <74705076+peni26553@users.noreply.github.com>
2020-11-23 10:19:28 +0100

    Add files via upload

---
+++ Romertal2/Romertal2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Romertal
+{               
+    class /*userInput? */program
+    {
+        static void Main(string[] args)
+        {
+            TextInput input = new TextInput();
+            input.Add('1');
+            input.Add('a');
+            input.Add('0');
+            Console.WriteLine(input.GetValue());
+        }
+    }
+}
