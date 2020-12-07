08c4eafdfca5bdc43b9b94a4d38285a587af9bdc
peni26553 <74705076+peni26553@users.noreply.github.com>
2020-12-07 21:09:40 +0100

    Add files via upload

--- WindowsFormsTALtilROMERTAL/WindowsFormsTALtilROMERTAL/Form1.Designer.cs
+++ WindowsFormsTALtilROMERTAL/WindowsFormsTALtilROMERTAL/Form1.Designer.cs
@@ -39,58 +39,61 @@ namespace WindowsFormsTALtilROMERTAL
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(310, 230);
+            this.button1.Location = new System.Drawing.Point(235, 257);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(280, 97);
             this.button1.TabIndex = 0;
-            this.button1.Text = "OMSKRIV TIL ";
+            this.button1.Text = "ROMERTAL GENERATOR";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(310, 130);
-            this.textBox1.Multiline = true;
+            this.textBox1.Location = new System.Drawing.Point(310, 183);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(280, 57);
+            this.textBox1.Size = new System.Drawing.Size(122, 34);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(312, 376);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Perpetua", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(199, 405);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(308, 65);
+            this.label1.Size = new System.Drawing.Size(360, 69);
             this.label1.TabIndex = 2;
-            this.label1.Text = "ROMERTAL";
+            this.label1.Text = "___________";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(209, 45);
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 94);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(481, 49);
+            this.label2.Size = new System.Drawing.Size(538, 56);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Skriv et årstal mellem 1 og 2030";
+            this.label2.Text = "SKRIV HUSETS BYGGEÅR";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Yellow;
-            this.BackgroundImage = global::WindowsFormsTALtilROMERTAL.Properties.Resources.ForumRomanum;
-            this.ClientSize = new System.Drawing.Size(1019, 576);
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.BackgroundImage = global::WindowsFormsTALtilROMERTAL.Properties.Resources.det_romerske_rige_kort_795x596;
+            this.ClientSize = new System.Drawing.Size(799, 602);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "GUL ROMERTAL GENERATOR";
+            this.Text = "ROMERTAL GENERATOR";
             this.ResumeLayout(false);
             this.PerformLayout();
 
